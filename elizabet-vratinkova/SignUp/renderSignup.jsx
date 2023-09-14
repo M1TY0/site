bfd2fdc88f3d1efc7e71fdff9a5c26f95485275f
Nikolay Hristov <114601022+nhristov777@users.noreply.github.com>
2023-09-14 14:58:35 +0300

    update 2

---
+++ elizabet-vratinkova/SignUp/renderSignup.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom"
+import SignUp from "./signup.jsx"
+
+ReactDOM.render(<SignUp/>, document.getElementById('signup'))
---
+++ elizabet-vratinkova/src/components/signup-page/Username.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function UsernameForm(){
+
+    return(
+        <div>
+            <label className="user--label" for="username">Enter Username:</label>
+            <input className="user--input" type="text" name="username"/>
+            <i className="fa-solid fa-user user--icon"></i>
+        </div>
+        
+    )
+}
